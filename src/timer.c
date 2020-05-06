f6bbc5b5b1efd212c53e91c81f13820050632b00
Andrew Miyaguchi <XDleader555@gmail.com>
2020-05-06 01:03:10 -0700

    Timer0 requires NVIC for interrupt vector

--- src/timer.c
+++ src/timer.c
@@ -2,6 +2,7 @@
 
 void (*_Timer0A_Handler)(void);
 
+// http://shukra.cedt.iisc.ernet.in/edwiki/EmSys:TM4C123_Timer_Programming
 void Timer_Init_Custom(uint8_t timer, void (*handler)(void), uint32_t reload) {
   // Assign internal handler for when the timer overflows
   _Timer0A_Handler = handler;
@@ -11,11 +12,13 @@ void Timer_Init_Custom(uint8_t timer, void (*handler)(void), uint32_t reload) {
   TIMER0_CTL_R    = 0x00000000;   // 1) disable TIMER0A during setup
   TIMER0_CFG_R    = 0x00000004;   // 2) configure for 16-bit mode
   TIMER0_TAMR_R   = 0x00000002;   // 3) configure for periodic mode, default down-count settings
-  TIMER0_TAILR_R  = 0x00000050;   // 4) count 80 clocks, or 1 us
+  TIMER0_TAILR_R  = reload - 1;   // 4) count 80 clocks, or 1 us
   TIMER0_TAPR_R   = 0x00000000;   // 5) no prescaler
   TIMER0_ICR_R    = 0x00000001;   // 6) clear TIMER0A timeout flag
   TIMER0_IMR_R    = 0x00000001;   // 7) arm timeout interrupt
-  TIMER0_CTL_R    = 0x00000001;   // 8) enable TIMER0A
+	NVIC_PRI4_R = (NVIC_PRI4_R&0x00FFFFFF)|0x40000000; // 8) priority 2
+  NVIC_EN0_R |= 0x00080000;    		// 9) enable interrupt 19 in NVIC
+  TIMER0_CTL_R    = 0x00000001;   // 10) enable TIMER0A
 }
 
 void Timer0A_Handler(void){
